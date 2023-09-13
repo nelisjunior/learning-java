41b8abc537c651d489ac48ed252c5f99ab76725a
Nelis Júnior <nelis.arruda.097@ufrn.edu.br>
2023-09-13 16:06:43 -0300

    Anatomia das Classes 3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,17 @@
 public class MinhaClasse{
 
-    public static void main (String [] args) {
-        System.out.print ("Olá turma, sejam bem-vindos");
-    }
+public static void main (String [] args) {
+
+    String primeiroNome = "Nelis";
+    String segundoNome = "Junior";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+    Sytem.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto(String primeiroNome, String segundoNome){
+    return "Nome completo: " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
 }
