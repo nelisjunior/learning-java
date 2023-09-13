76726a032c8f792861844bbcce4925f61c756f90
Nelis Júnior <nelis.arruda.097@ufrn.edu.br>
2023-09-13 16:23:17 -0300

    Tipos e Variaveis

---
+++ java-anatomia-classes/src/tiposVariaveis.java
@@ -0,0 +1,20 @@
+public class MinhaClasse{
+
+    public static void main (String [] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        Sytem.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+    }
+
+}
